4e1803e5ed786c8a2109ed80ae4ec3f406846ffc
mayank-ramnani <mayankr99@gmail.com>
2025-04-08 18:53:35 -0400

    fix init so that types don't need to be imported separately

--- taskboard/__init__.py
+++ taskboard/__init__.py
@@ -1,4 +1,7 @@
 from .interface import TaskBoard
+from .types import Task, TaskList
 from collections.abc import Callable
 
 get_client: Callable[[], TaskBoard] = lambda: NotImplemented
+
+__all__ = ["TaskBoard", "Task", "TaskList", "get_client"]
